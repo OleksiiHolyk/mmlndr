b1ba0cb11b16825dec7967a80bd7775c494f4471
Oleksii Holyk <oleksii.holyk@outlook.com>
2017-11-09 16:46:11 +0200

    - add calendar creation functions - add HTML version of calendar (without fill) - add get period between dates - add get date -2week of TODAY - remove moment js functions (go to pure js)

--- src/main/resources/static/js/tempage.js
+++ src/main/resources/static/js/tempage.js
@@ -1,11 +1,57 @@
 /**
  * Created by Oleksii on 08.11.2017.
  */
+function addCurrentDateToCalendar(date) {
+
+    /**
+     * implementation using moment.js
+     *
+     * var momentDay = moment(date);
+     * var dayOfWeek = momentDay.day();
+     * var dayOfMonth = momentDay.date();
+     * */
+
+    var dom = date.getDate();   //date of month
+    var dow = date.getDay();    //date of week
+    console.log('dayOfWeek=' + dom);
+    $('tr.secondWeek').find('td:eq(' + (dow - 1) + ')').text(dom);
+}
+
+function getPeriod(currentDate) {
+    Date.prototype.addDays = function (days) {
+        var dat = new Date(this.valueOf());
+        dat.setDate(dat.getDate() + days);
+        return dat;
+    };
+
+    function getDates(dateFrom, dateTo) {
+        var dateArray = [];
+        while (dateFrom <= dateTo) {
+            dateArray.push(dateFrom);
+            dateFrom = dateFrom.addDays(1);
+        }
+        return dateArray;
+    }
+
+
+    var prevDate = new Date(currentDate);
+    prevDate.setDate(prevDate.getDate() - 14); // minus the date
+
+    var dateArray = getDates(prevDate, currentDate);
+    for (i = 0; i < dateArray.length; i++) {
+        console.log(dateArray[i]);
+    }
+}
+
 $(document).ready(function () {
     $("button#getCurrentDate").click(function () {
         var today = new Date();
-        var momentDay = moment(today);
-        var dayOfWeek = momentDay.day();
-        console.log(dayOfWeek);
+        addCurrentDateToCalendar(today);
+        getPeriod(today);
+        console.log('today=' + today)
     })
 });
+
+
+
+
